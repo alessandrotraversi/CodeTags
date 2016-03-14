60c47e31b989f7f2c80a95ab44d1406a71b9b38f
alessandrotraversi <alessandro.traversi@hotmail.it>
2016-03-14 20:42:12 -0300

    correzioni

--- src/resources/migrations/2016_03_05_1229_CreateCodeTagsTable.php
+++ src/resources/migrations/2016_03_05_1229_CreateCodeTagsTable.php
@@ -10,7 +10,7 @@ class CreateCodeTagsTable
            $table->increments('id');
             $table->string('name');
             $table->string('slug');
-            $table->timestamp();
+            $table->timestamps();
         });
     }
 
